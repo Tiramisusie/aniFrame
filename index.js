fe9c2f8a4a396f5f3e215f89ce36a27cd47fc2ff
梁宁聪 <liangningcong@maoyan.com>
2016-11-01 15:43:19 +0800

    添加帧函数

---
+++ index.js
@@ -0,0 +1,20 @@
+var nextFrame = (function() {
+		return window.requestAnimationFrame ||
+			window.webkitRequestAnimationFrame ||
+			window.mozRequestAnimationFrame ||
+			window.oRequestAnimationFrame ||
+			window.msRequestAnimationFrame ||
+			function(callback) {
+				return setTimeout(callback, 1);
+			};
+	})(),
+
+	cancelFrame = (function() {
+		return window.cancelRequestAnimationFrame ||
+			window.webkitCancelAnimationFrame ||
+			window.webkitCancelRequestAnimationFrame ||
+			window.mozCancelRequestAnimationFrame ||
+			window.oCancelRequestAnimationFrame ||
+			window.msCancelRequestAnimationFrame ||
+			clearTimeout;
+	})();
